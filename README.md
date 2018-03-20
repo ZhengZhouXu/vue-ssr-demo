3685977233f44cd944748aa20600bbb3e4de8da1
xuxule <819384406@qq.com>
2018-03-20 13:40:05 +0800

    异步问题解决

--- README.md
+++ README.md
@@ -8,4 +8,11 @@ localhost:8989/
 localhost:8989/bar
 
 ### 问题
-异步组件用不了，有些问题待解决
+异步组件必需使用babel插件  
+1.npm
+> npm install --save-dev babel-plugin-syntax-dynamic-import  
+
+2.babelrc
+> {
+  "plugins": ["syntax-dynamic-import"]
+}
--- router.js
+++ router.js
@@ -1,25 +1,8 @@
-// import Vue from 'vue'
-// import Router from 'vue-router'
-
-// Vue.use(Router)
-// export function createRouter () {
-//   return new Router({
-//     mode: 'history',
-//     routes: [{
-//       path: '/',
-//       component: () => require('./components/Home.vue')
-//     }, {
-//       path: '/bar',
-//       component: () => require('./components/Bar.vue')
-//     }]
-//   })
-// }
-
 // router.js
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './components/Home.vue'
-import Bar from './components/Bar.vue'
+// import Bar from './components/Bar.vue'
 
 Vue.use(Router)
 export function createRouter () {
@@ -30,7 +13,8 @@ export function createRouter () {
       component: Home
     }, {
       path: '/bar',
-      component: Bar
+      // 这么写必需安装babel插件: babel-plugin-syntax-dynamic-import
+      component: () => import('./components/Bar.vue') 
     }]
   })
 }
